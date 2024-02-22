60949d4b4ffc937b2b763b2e91021b27d6c9fc47
m-shehzad455 <shehzuali5@gmail.com>
2024-02-22 21:57:41 +0500

    update

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+<<<<<<< main
 const AuthLayout = () => {
   return (
     <div>AuthLayout</div>
@@ -7,3 +8,30 @@ const AuthLayout = () => {
 }
 
 export default AuthLayout
+=======
+import { useUserContext } from "@/context/AuthContext";
+
+export default function AuthLayout() {
+  const { isAuthenticated } = useUserContext();
+
+  return (
+    <>
+      {isAuthenticated ? (
+        <Navigate to="/" />
+      ) : (
+        <>
+          <section className="flex flex-1 justify-center items-center flex-col py-10">
+            <Outlet />
+          </section>
+
+          <img
+            src="/assets/images/side-img.svg"
+            alt="logo"
+            className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
+          />
+        </>
+      )}
+    </>
+  );
+}
+>>>>>>> local
